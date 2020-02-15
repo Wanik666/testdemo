cbac1358630acb98ab682763ea43d72415d3339b
wanik <248264087@qq.com>
2020-02-15 20:33:28 +0800

    branch2 commit HelloWord.java

---
+++ HelloWord.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+   
+    public static void main(String []args) {
+        System.out.println("111");
+		System.out.println("222");
+		System.out.println("333");
+
+    }
+}
